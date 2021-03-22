9e5fa3867c429ad577f9c9f36bb0e9d9f139650b
BadMusician <dev@ykmail.ru>
2021-03-22 13:13:36 +0300

    Drilling

---
+++ ScaleformsResearch/Movies/Drilling.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScaleformsResearch.Movies
+{
+    class Drilling : Movie
+    {
+        public override string MovieName => "DRILLING";
+
+        private float speed = 0, depth = 0, temp = 0;
+
+        public float Speed { get => speed; set { speed = value.Clamp(0, 1); CallFunction("SET_SPEED", speed); } }
+        public float Depth { get => depth; set { depth = value.Clamp(0, 1); CallFunction("SET_DEPTH", depth); } }
+        public float Temperature { get => temp; set { temp = value.Clamp(0, 1); CallFunction("SET_TEMPERATURE", temp); } }
+    }
+}
---
+++ ScaleformsResearch/Movies/Util.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScaleformsResearch.Movies
+{
+    internal static class Util
+    {
+        public static T Clamp<T>(this T x, T min, T max) where T : IComparable<T> => Rage.MathHelper.Clamp(x, min, max);
+    }
+}
