4c66ebc31dd8802cacb250f4c5f14b621ccb4c44
BadMusician <dev@ykmail.ru>
2021-03-22 15:09:33 +0300

    Added 'Binoculars', 'BlimpText'

---
+++ ScaleformsResearch/Movies/Binoculars.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScaleformsResearch.Movies
+{
+    class Binoculars : Movie
+    {
+        public override string MovieName => "BINOCULARS";
+    }
+}
