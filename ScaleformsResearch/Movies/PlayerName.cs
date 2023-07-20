fbf50be0953fa7566320df5c217d6ffda9be61d4
Vincentsgm <angosyl@gmail.com>
2023-07-20 17:28:09 +0200

    Reimplement SET_SPEAKER_STATE for PLAYER_NAME scaleforms

--- ScaleformsResearch/Movies/PlayerName.cs
+++ ScaleformsResearch/Movies/PlayerName.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Rage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ScaleformsResearch.Movies
 {
@@ -11,12 +13,33 @@ namespace ScaleformsResearch.Movies
         public override string MovieName => "PLAYER_NAME_01";
 
         public string PlayerNameStr { set => CallFunction("SET_PLAYER_NAME", value); }
-        public bool SpeakerState { set => CallFunction("SET_SPEAKER_STATE", value); }
+        public ESpeakerState SpeakerState { set => CallFunction("SET_SPEAKER_STATE", (int)value); }
 
         protected override void OnTestStart()
         {
-            PlayerNameStr = MovieName;
-            SpeakerState = true;
+            PlayerNameStr = $"{MovieName}: ~b~{Game.LocalPlayer.Name}";
+        }
+
+        protected override void OnTestTick()
+        {
+            base.OnTestTick();
+            if (Game.IsKeyDown(Keys.NumPad0)) SpeakerState = ESpeakerState.NoHeadset;
+            else if (Game.IsKeyDown(Keys.NumPad1)) SpeakerState = ESpeakerState.Active;
+            else if (Game.IsKeyDown(Keys.NumPad2)) SpeakerState = ESpeakerState.Speaking;
+            else if (Game.IsKeyDown(Keys.NumPad3)) SpeakerState = ESpeakerState.Muted;
+        }
+
+        protected override string TestHelpMessage => $"~s~No Headset: ~y~NumPad0\n" +
+            $"~s~Active Headset: ~y~NumPad1\n" +
+            $"~s~Speaking Headset: ~y~NumPad2\n" +
+            $"~s~Muted Headset: ~y~NumPad3\n";
+
+        public enum ESpeakerState
+        {
+            NoHeadset = 1,
+            Active = 2,
+            Speaking = 3,
+            Muted = 4
         }
     }
 
