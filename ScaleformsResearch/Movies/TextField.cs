3ab2b9d9ee2872a345a0f0b83645713d6b78fb73
Vincentsgm <angosyl@gmail.com>
2023-07-20 18:27:30 +0200

    Add TEXTFIELD

---
+++ ScaleformsResearch/Movies/TextField.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScaleformsResearch.Movies
+{
+    internal class TextField : Movie
+    {
+        public override string MovieName => "TEXTFIELD";
+        public string Text { set => CallFunction("SET_TEXT", value); } 
+        public float TextPointSize { set => CallFunction("SET_TEXT_POINT_SIZE", value); }
+        public void SetBackgroundImage(string txd, string txn, float alpha) => CallFunction("SET_BACKGROUND_IMAGE", txd, txn, alpha);
+        public void ClearBackgroundImage() => CallFunction("CLEAR_BACKGROUND_IMAGE");
+
+        protected override void OnTestStart()
+        {
+            base.OnTestStart();
+            Text = "Test text";
+            //SetBackgroundImage("www_stoppayingyourmortgage_net", "image_background_header", 255f);
+            ClearBackgroundImage();
+            TextPointSize = 10f;
+        }
+    }
+}
---
+++ ScaleformsResearch/Movies/TickerTape.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScaleformsResearch.Movies
+{
+    //This scaleform does not seem to do anything
+    internal class TickerTape : Movie
+    {
+        public override string MovieName => "TICKERTAPE";
+
+        public string TickerText { set => CallFunction("SET_TICKER_TEXT", 1f, value); }
+
+        protected override void OnTestStart()
+        {
+            base.OnTestStart();
+            TickerText = "Test";
+        }
+    }
+}
